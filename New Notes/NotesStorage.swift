f11ff420670108be331f1c76b26ea524c333c49c
Artem Karmaz <inste@icloud.com>
2019-02-07 14:07:56 +0200

    seventh commit

--- New Notes/NotesStorage.swift
+++ New Notes/NotesStorage.swift
@@ -10,11 +10,12 @@ import Foundation
 
 struct Storage {
      var notes = [
-        Note(text: "Shopping List\r\r1. Cheese\r2. Biscuits\r3. Sausages\r4. IMPORTANT Cash for going out!\r5. -potatoes-\r6. A copy of iOS8 by Tutorials\r7. A new iPhone\r8. A present for mum"),
-        Note(text: "Meeting notes\rA long and drawn out meeting, it lasted hours and hours and hours!"),
-        Note(text: "Perfection ... \n\nPerfection is achieved not when there is nothing left to add, but when there is nothing left to take away - Antoine de Saint-Exupery"),
+        Note(text: "What notes can do?\r\r 1. two ~ for beatiful headline \r 2. two * for bold font \r 3. two spacebar for ALERT \r 4. two - for crossed out text\r 5. two _ for titled text\r 6. number with . for enumerations\r 7. Cool searchbar in main menu"),
+        Note(text: "~SHOPPING LIST~\r\r1. Apple watch\r2. iPhone 8\r3. iMac new\r4. IMPORTANT Cash for going out!\r5. -macbook pro-\r6. *money?*\r8. _A present for mum_"),
+        Note(text: "Car\rA My car is very beatiful!"),
+        Note(text: "iOS12 \n\niOS12 More faster than iOS 11\r 1. -Install iOS12-"),
         Note(text: "Notes on Swift\nThis new language from Apple is changing iOS development as we know it!"),
-        Note(text: "Meeting notes\rA different meeting, just as long and boring"),
-        Note(text: "A collection of thoughts\rWhy do birds sing? Why is the sky blue? Why is it so hard to create good test data?")
+        Note(text: "A collection of thoughts\rWhy do birds sing? Why is the sky blue? Why is it so hard to create good test data?\r DAMN "),
+        Note(text: "*Call Jonny*\r-saturday- \r_sunday_")
     ]
 }
