65c3b88fd21b3ce97006c1730e2de0a286efd376
Artem Karmaz <inste@icloud.com>
2019-02-07 12:10:09 +0200

    Initial Commit

---
+++ New Notes/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  New Notes
+//
+//  Created by Artem Karmaz on 2/7/19.
+//  Copyright © 2019 Johansson Group. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
